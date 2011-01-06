c24172c8e2c9dfb6300b1ee53e69c58ee8a6383a
Michael Petritsch <michael.petritsch@gmail.com>
2011-01-06 18:09:39 +0100

    parameterized test
    Signed-off-by: Michael Petritsch <michael.petritsch@gmail.com>

--- test/at/ticketline/dao/ReservierungErstellenTest.java
+++ test/at/ticketline/dao/ReservierungErstellenTest.java
@@ -1,32 +1,54 @@
 package at.ticketline.dao;
 
-import com.thoughtworks.selenium.*;
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import java.util.regex.Pattern;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+import com.thoughtworks.selenium.DefaultSelenium;
+import com.thoughtworks.selenium.SeleneseTestCase;
+
+@RunWith(value = Parameterized.class)
 public class ReservierungErstellenTest extends SeleneseTestCase {
-	@Before
-	public void setUp() throws Exception {
-		selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080/tllight/");
-		selenium.start();
-	}
 
-	@Test
-	public void testReservierungErstellen() throws Exception {
-		selenium.open("/tllight");
-		selenium.click("veranstaltung_1");
-		selenium.click("auffuehrung_1");
-		selenium.click("platz_1_4");
-		selenium.click("executeReservierung");
-		verifyTrue(selenium.isTextPresent("Die Reservierung wurde erfolgreich durchgeführt"));
-		selenium.click("link=Veranstaltungen");
-		selenium.click("//li[5]/a");
-	}
+    private int number;
 
-	@After
-	public void tearDown() throws Exception {
-		selenium.stop();
-	}
+    public ReservierungErstellenTest(int number) {
+        this.number = number;
+    }
+
+    @Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] { { 4 }, { 5 }, { 6 }, { 7 } };
+        return Arrays.asList(data);
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        selenium = new DefaultSelenium("localhost", 4444, "*firefox", "http://localhost:8080/tllight/");
+        selenium.setSpeed("100");
+        selenium.start();
+    }
+
+    @Test
+    public void testReservierungErstellen() throws Exception {
+        selenium.open("/tllight");
+        selenium.click("veranstaltung_1");
+        selenium.click("auffuehrung_1");
+        selenium.click("platz_1_" + number);
+        selenium.click("executeReservierung");
+        verifyTrue(selenium.isTextPresent("Die Reservierung wurde erfolgreich durchgeführt"));
+        selenium.click("link=Veranstaltungen");
+        selenium.click("//li[5]/a");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        selenium.stop();
+    }
 }
